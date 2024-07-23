3ddc9cab076ceb0ff0c499d15846f8a5f55325bb
Thanh Nguyen <thanh.nguyen@cavallo.com>
2024-07-23 17:14:51 -0400

    feat: add hook example

---
+++ src/components/CallbackDemo/customHookDemo.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useState } from "react";
+import { DemoObject } from "../../shared/utils/object";
+import { Button } from "./button";
+
+type HookProps = {
+  counter: number;
+  setCounter: (value: number) => void;
+  toggle: boolean;
+  setToggle: (value: boolean) => void;
+};
+
+const useUpdateComponent = ({
+  counter,
+  setCounter,
+  toggle,
+  setToggle,
+}: HookProps) => {
+  const updateCounter = useCallback(() => {
+    setCounter(counter + 1);
+  }, [counter]);
+
+  const updateToggle = useCallback(() => {
+    setToggle(!toggle);
+  }, [toggle]);
+
+  return { updateCounter, updateToggle };
+};
+
+export const CustomHookDemo = () => {
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const contextObject = new DemoObject(); // 64MB
+
+  const { updateCounter, updateToggle } = useUpdateComponent({
+    counter,
+    setCounter,
+    toggle,
+    setToggle,
+  });
+
+  const handleCounterButtonClick = () => {
+    updateCounter();
+  };
+
+  const handleToggleButtonClick = () => {
+    updateToggle();
+  };
+
+  const getObjectSize = () => {
+    return contextObject.data.length;
+  };
+
+  return (
+    <div>
+      <h1>Hook Demo</h1>
+      <div>Counter: {counter}</div>
+      <div>Toggle Status: {`${toggle}`}</div>
+      <Button onClick={handleCounterButtonClick}>Increment</Button>
+      <Button onClick={handleToggleButtonClick}>Toggle</Button>
+      <div>Demo object size: {getObjectSize()}</div>
+    </div>
+  );
+};
